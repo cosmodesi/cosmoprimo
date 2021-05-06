1d39d2859f2f7c58274cbf6a21a557d948ee77ae
adematti <arnaud.de-mattia@cea.fr>
2021-05-06 15:21:02 +0200

    add __all__ in init

--- cosmoprimo/__init__.py
+++ cosmoprimo/__init__.py
@@ -4,3 +4,9 @@ from .cosmology import Planck2018FullFlatLCDM
 from .interpolator import PowerSpectrumInterpolator1D, PowerSpectrumInterpolator2D, CorrelationFunctionInterpolator1D, CorrelationFunctionInterpolator2D
 from .fftlog import FFTlog, PowerToCorrelation, CorrelationToPower, TophatVariance
 from .bao_filter import PowerSpectrumBAOFilter, CorrelationFunctionBAOFilter
+
+__all__ = ['Cosmology', 'Background', 'Thermodynamics', 'Primordial', 'Transfer', 'Harmonic', 'Fourier', 'CosmologyError']
+__all__ += ['Planck2018FullFlatLCDM']
+__all__ += ['PowerSpectrumInterpolator1D', 'PowerSpectrumInterpolator2D', 'CorrelationFunctionInterpolator1D', 'CorrelationFunctionInterpolator2D']
+__all__ += ['FFTlog', 'PowerToCorrelation', 'CorrelationToPower', 'TophatVariance']
+__all__ += ['PowerSpectrumBAOFilter', 'CorrelationFunctionBAOFilter']
