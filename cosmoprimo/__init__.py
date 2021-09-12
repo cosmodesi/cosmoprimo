cbd92c3825b5cae286683e6de9ab6876165aba7f
Arnaud De-Mattia <adematti@spppcx147.extra.cea.fr>
2021-09-12 17:23:44 +0200

    updating notebook

--- cosmoprimo/__init__.py
+++ cosmoprimo/__init__.py
@@ -3,8 +3,10 @@ from .cosmology import Cosmology, Background, Thermodynamics, Primordial, Transf
 from .interpolator import PowerSpectrumInterpolator1D, PowerSpectrumInterpolator2D, CorrelationFunctionInterpolator1D, CorrelationFunctionInterpolator2D
 from .fftlog import FFTlog, PowerToCorrelation, CorrelationToPower, TophatVariance
 from .bao_filter import PowerSpectrumBAOFilter, CorrelationFunctionBAOFilter
+from . import fiducial
 
 __all__ = ['Cosmology', 'Background', 'Thermodynamics', 'Primordial', 'Transfer', 'Harmonic', 'Fourier', 'CosmologyError']
 __all__ += ['PowerSpectrumInterpolator1D', 'PowerSpectrumInterpolator2D', 'CorrelationFunctionInterpolator1D', 'CorrelationFunctionInterpolator2D']
 __all__ += ['FFTlog', 'PowerToCorrelation', 'CorrelationToPower', 'TophatVariance']
 __all__ += ['PowerSpectrumBAOFilter', 'CorrelationFunctionBAOFilter']
+__all__ += ['fiducial']
