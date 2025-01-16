74ea5b9dd14b441d3761ad84414fca5dc695a068
arnaud de mattia <adematti@dphppcz231.extra.cea.fr>
2025-01-16 10:23:54 +0100

    environment variable for emulator directory

--- cosmoprimo/emulators/hybrid.py
+++ cosmoprimo/emulators/hybrid.py
@@ -5,7 +5,11 @@ from pathlib import Path
 from .emulated import EmulatedEngine, Thermodynamics, Harmonic
 
 
-train_dir = Path(__file__).parent / 'train'
+def get_train_dir():
+    dirname = os.getenv('COSMOPRIMO_EMULATOR_DIR', '')
+    if not dirname:
+        dirname = Path(__file__).parent
+    return dirname / 'train'
 
 
 from cosmoprimo.cosmology import DefaultBackground
@@ -19,11 +23,11 @@ class Background(DefaultBackground):
 class CAPSEEngine(EmulatedEngine):
 
     name = 'capse'
-    path = {train_dir / 'camb_base_mnu_w_wa/emulator_thermodynamics.npy': 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/camb_base_mnu_w_wa/emulator_thermodynamics.npy',
-            train_dir / 'jaxcapse_base_mnu_w_wa/emulator.npy': 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/jaxcapse_base_mnu_w_wa/emulator.npy'}
+    path = {get_train_dir() / 'camb_base_mnu_w_wa/emulator_thermodynamics.npy': 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/camb_base_mnu_w_wa/emulator_thermodynamics.npy',
+            get_train_dir() / 'jaxcapse_base_mnu_w_wa/emulator.npy': 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/jaxcapse_base_mnu_w_wa/emulator.npy'}
 
 
 class CosmopowerBolliet2023Engine(EmulatedEngine):
 
     name = 'cosmopower_bolliet2023'
-    path = {train_dir / 'cosmopower_bolliet2023_base_mnu/emulator_{}.npy'.format(section): 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/cosmopower_bolliet2023_base_mnu/emulator_{}.npy'.format(section) for section in ['thermodynamics', 'harmonic', 'fourier']}
+    path = {get_train_dir() / 'cosmopower_bolliet2023_base_mnu/emulator_{}.npy'.format(section): 'https://github.com/adematti/cosmoprimo-emulators/raw/refs/heads/main/cosmopower_bolliet2023_base_mnu/emulator_{}.npy'.format(section) for section in ['thermodynamics', 'harmonic', 'fourier']}
